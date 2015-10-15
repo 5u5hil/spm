8a24fc0a1ff5faa2bb6f0797fa8e2790412d7ffc
sushilcs111 <sushil.sud@gmail.com>
2015-10-15 09:40:05 +0530

    lol

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,5 +1,10 @@
 document.addEventListener('deviceready', function () {
+    
+    if(device.platform == "iOS"){
     initPushwoosh();
+} else {
+    initPushwooshAndroid();
+}
 
 
 }, false);
@@ -40,7 +45,35 @@ function initPushwoosh() {
 
 }
 
-
+function  initPushwooshAndroid(){
+    var pushNotification = cordova.require("com.pushwoosh.plugins.pushwoosh.PushNotification");
+ 
+    //set push notifications handler
+    document.addEventListener('push-notification', function(event) {
+        var title = event.notification.title;
+        var userData = event.notification.userdata;
+                                 
+        if(typeof(userData) != "undefined") {
+            console.warn('user data: ' + JSON.stringify(userData));
+        }
+                                     
+        alert(title);
+    });
+ 
+    //initialize Pushwoosh with projectid: "GOOGLE_PROJECT_ID", pw_appid : "PUSHWOOSH_APP_ID". This will trigger all pending push notifications on start.
+    pushNotification.onDeviceReady({ projectid: "830986347372", pw_appid : "03DBD-4FF6F" });
+ 
+    //register for pushes
+    pushNotification.registerDevice(
+        function(status) {
+            var pushToken = status;
+            console.warn('push token: ' + pushToken);
+        },
+        function(status) {
+            console.warn(JSON.stringify(['failed to register ', status]));
+        }
+    );
+}
 
 
 
