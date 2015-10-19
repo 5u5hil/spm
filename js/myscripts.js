d6ab836837cd89ed649671cd52a6219a53f36782
tapoo <tapodnya@infiniteit.biz>
2015-10-19 19:29:15 +0530

    sdfss

--- js/myscripts.js
+++ js/myscripts.js
@@ -152,14 +152,10 @@ function checkPLogin() {
         var r = confirm("Are you sure you want to subscribe to SP and be a Member?");
         if (r == true) {
             jQuery.get(domain + "/update-membership?userId=" + window.localStorage.getItem('id')).success(function (data, status, headers, config) {
-                //            window.localStorage.setItem('member', 1);
-                console.log("o:"+data);
-                if (data == "saved") {
-                    console.log('hi');
-                    window.localStorage.setItem('department', 'Karishma');
-                    window.open("http://sp.boxcommerce.in/personal-chat.php?name=" + window.localStorage.getItem("name") + "&email=" + window.localStorage.getItem("email") + "&dep=" + window.localStorage.getItem("department"), '_blank', 'EnableViewPortScale=yes,location=no,closebuttoncaption=Close');
-                }
+                window.location.href = "#/questionnaire";
             });
+//            window.localStorage.setItem('member', 1);
+//            window.open("http://sp.boxcommerce.in/personal-chat.php?name=" + window.localStorage.getItem("name") + "&email=" + window.localStorage.getItem("email") + "&dep=" + window.localStorage.getItem("department"), '_blank', 'EnableViewPortScale=yes,location=no,closebuttoncaption=Close');
         }
     } else {
         window.open("http://sp.boxcommerce.in/personal-chat.php?name=" + window.localStorage.getItem("name") + "&email=" + window.localStorage.getItem("email") + "&dep=" + window.localStorage.getItem("department"), '_blank', 'EnableViewPortScale=yes,location=no,closebuttoncaption=Close');
