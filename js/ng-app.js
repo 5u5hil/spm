cc4aa7014802fee83a918e0c867ebae163f2c0ae
tapoo <tapodnya@infiniteit.biz>
2015-10-29 13:29:10 +0530

    kfkf

--- js/ng-app.js
+++ js/ng-app.js
@@ -104,8 +104,19 @@ app.controller('categoryController', function ($http, $scope, $location, $rootSc
         });
         $anchorScroll();
         console.log('hi');
-        $scope.$on("$locationChangeSuccess", function () {
-            $anchorScroll();
+
+        $scope.run(function ($anchorScroll, $window) {
+            // hack to scroll to top when navigating to new URLS but not back/forward
+            var wrap = function (method) {
+                var orig = $window.window.history[method];
+                $window.window.history[method] = function () {
+                    var retval = orig.apply(this, Array.prototype.slice.call(arguments));
+                    $anchorScroll();
+                    return retval;
+                };
+            };
+            wrap('pushState');
+            wrap('replaceState');
         });
     };
 
